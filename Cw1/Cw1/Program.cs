bd1dfe76552bd1e33985b78cbff2d38df8a6f211
Jakub <kazmierczak.jakubb@gmail.com>
2020-04-21 19:56:13 +0200

    Zadanie 4 oraz 5

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace Cw1
@@ -14,17 +15,44 @@ namespace Cw1
 
         public static async System.Threading.Tasks.Task Main(string[] args)
         {
+            if (args.Length < 1)
+            {
+                throw new ArgumentNullException();
+            }
+            else if (!isUrlValid(args[0]))
+            {
+                throw new ArgumentException();
+            }
+
             var httpClient = new System.Net.Http.HttpClient();
             var response = await httpClient.GetAsync(args[0]);
-
+            
             var body = response.Content.ReadAsStringAsync();
-           
             var regex = new Regex(emailRegex, RegexOptions.Compiled | RegexOptions.IgnoreCase);
-
+            var emails = new HashSet<string>();
             foreach (Match match in regex.Matches(body.Result))
             {
-                Console.WriteLine(match.Value.ToString());
+                emails.Add(match.Value.ToString());
             }
+            if (emails.Count == 0)
+            {
+                Console.WriteLine("Ńie znaleziono adresów email");
+            }
+            else
+            {
+
+                foreach (string email in emails)
+                {
+                    Console.WriteLine(email);
+                }
+            }
+        }
+
+        private static bool isUrlValid(string url)
+        {
+            Uri result;
+            return Uri.TryCreate(url, UriKind.Absolute, out result)
+                && (result.Scheme == Uri.UriSchemeHttp || result.Scheme == Uri.UriSchemeHttps);
         }
     }
 }
